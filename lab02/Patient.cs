1221b59a7427bd15df60fbcb8a19f631e4601c06
akrzelj-ossunist <krzelj99antonio@gmail.com>
2024-03-12 23:40:25 +0100

    lab02

---
+++ lab02/Patient.cs
@@ -0,0 +1,35 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+public class Patient
+{
+    [StringLength(11, ErrorMessage = "OIB must be 11 characters long")]
+    public string OIB { get; set; }
+
+    [StringLength(9, ErrorMessage = "MBO must be 9 characters long")]
+    public string MBO { get; set; }
+
+    public string FullName { get; set; }
+
+    public DateTime Birthday { get; set; }
+
+    public string Gender { get; set; }
+
+    public string Diagnosis { get; set; }
+
+    public Patient(string OIB, string MBO, string FullName, DateTime Birthday, string Gender, string Diagnosis)
+    {
+        if (OIB.Length != 11)
+            throw new ArgumentException("OIB must be 11 characters long.");
+
+        if (MBO.Length != 9)
+            throw new ArgumentException("MBO must be 9 characters long.");
+
+        this.OIB = OIB;
+        this.MBO = MBO;
+        this.FullName = FullName;
+        this.Birthday = Birthday;
+        this.Gender = Gender;
+        this.Diagnosis = Diagnosis;
+    }
+}
---
+++ lab02/Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+WPF stands for Windows Presentation Foundation.
+It is a graphical subsystem developed by Microsoft for rendering user interfaces in Windows-based applications.
+WPF is part of the .NET Framework and provides a unified programming model for building desktop applications, 
+including rich user interfaces, graphics, multimedia, and documents.
+*/
+class Program
+{
+    public static void Main()
+    {
+        Patient mate = new("12345678910", "123456789", "Mate Matic", DateTime.Parse("1990-05-15"), "Male", "Sick AF");
+
+        Patient ana = new("12345678912", "123456743", "Ana Anic", DateTime.Parse("1996-01-16"), "Female", "Dont wanna go to work!");
+
+        Patient jimmy = new("43345678942", "126556743", "Jimmy Johnny", DateTime.Parse("4996-01-16"), "Turtle", "Unknown!");
+
+        Hospital hospital = new();
+
+        hospital.Add(mate);
+        hospital.Add(ana);
+        hospital.Add(jimmy);
+
+        hospital.Print();
+
+        hospital.Remove(ana.OIB);
+
+        hospital.Print();
+
+        jimmy.Gender = "NotTurtle";
+
+        hospital.Edit(jimmy);
+
+        hospital.Print();
+    }
+}
